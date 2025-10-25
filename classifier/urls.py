b24e09d4f224784b032d39e9bd49fcae17d0cc9f
MohamedAlaaAhmed <mohamedalaa2405@gmail.com>
2025-10-25 18:47:27 +0300

    Delete classifier directory

--- classifier/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_video, name='upload_video'),
-    path('videos/', views.video_list, name='video_list'),
-    path('video/<int:pk>/', views.video_detail, name='video_detail'),
-    path('delete/<int:pk>/', views.delete_video, name='delete_video'),
-]
